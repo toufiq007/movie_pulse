6fcb17aa4e3963147304d7b2b0b5cd6b964d1ec1
toufiq007 <t.limon2342@gmail.com>
2024-10-25 23:31:30 +0600

    created sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,63 @@
+import Trending from "../assets/assets/icons/trending.svg";
+import NewRelease from "../assets/assets/icons/newRelease.svg";
+import CommingSoon from "../assets/assets/icons/commingSoon.svg";
+import Favorites from "../assets/assets/icons/favourite.svg";
+import WatchLater from "../assets/assets/icons/watchLater.svg";
+
+const Sidebar = () => {
+  return (
+    <>
+      <aside>
+        <ul className="space-y-2">
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg bg-primary text-black"
+              href="#"
+            >
+              <img src={Trending} width="24" height="24" alt="" />
+              <span>Trending</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img src={NewRelease} width="24" height="24" alt="" />
+              <span>New Releases</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img src={CommingSoon} width="24" height="24" alt="" />
+              <span>Coming Soon</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img src={Favorites} width="24" height="24" alt="" />
+              <span>Favourites</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img src={WatchLater} width="24" height="24" alt="" />
+              <span>Watch Later</span>
+            </a>
+          </li>
+        </ul>
+      </aside>
+    </>
+  );
+};
+
+export default Sidebar;
