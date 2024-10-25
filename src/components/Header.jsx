b620bc14a1c0584dd4eb39ca8f24502ef44968ca
toufiq007 <t.limon2342@gmail.com>
2024-10-25 23:26:22 +0600

    header component is created

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import Logo from "../assets/assets/logo.svg";
+import Ring from "../assets/assets/ring.svg";
+import Moon from "../assets/assets/icons/moon.svg";
+import ShoppingCart from "../assets/assets/shopping-cart.svg";
+const Header = () => {
+  return (
+    <header>
+      <nav className="container flex items-center justify-between space-x-10 py-6">
+        <a href="index.html">
+          <img src={Logo} width="139" height="26" alt="" />
+        </a>
+
+        <ul className="flex items-center space-x-5">
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={Ring} width="24" height="24" alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={Moon} width="24" height="24" alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={ShoppingCart} width="24" height="24" alt="" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
