45ac3143d7c93fa2e6be7c3da02b3263c27d0c2d
toufiq007 <t.limon2342@gmail.com>
2024-10-26 00:17:03 +0600

    breakdown movieList page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,6 @@
-const Movie = () => {
+/* eslint-disable react/prop-types */
+import Ratting from "./Ratting";
+const MovieCard = ({ movieData }) => {
   return (
     <>
       <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
@@ -8,21 +10,20 @@ const Movie = () => {
           alt=""
         />
         <figcaption className="pt-4">
-          <h3 className="text-xl mb-1">Iron Man</h3>
-          <p className="text-[#575A6E] text-sm mb-2">Action/Adventure/Sci-fi</p>
+          <h3 className="text-xl mb-1">{movieData?.title}</h3>
+          <p className="text-[#575A6E] text-sm mb-2">{movieData?.genre}</p>
           <div className="flex items-center space-x-1 mb-5">
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
-            <img src="./assets/star.svg" width="14" height="14" alt="" />
+            {
+              
+            }
+            <Ratting />
           </div>
           <a
             className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
             href="#"
           >
             <img src="./assets/tag.svg" alt="" />
-            <span>$100 | Add to Cart</span>
+            <span>${movieData?.price} | Add to Cart</span>
           </a>
         </figcaption>
       </figure>
@@ -30,4 +31,4 @@ const Movie = () => {
   );
 };
 
-export default Movie;
+export default MovieCard;
---
+++ src/components/Ratting.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import Star from "../assets/assets/star.svg";
+const Ratting = ({ startNumber }) => {
+  return (
+    <>
+      <img src={Star} width="14" height="14" alt="" />
+    </>
+  );
+};
+
+export default Ratting;
